90f4571a58c29676627ebf0b04c9b339c8af4a02
Peru Bhardwaj <peru.bhardwaj@gmail.com>
2015-12-17 13:31:45 +0000

    work
    work

---
+++ SampleApplication/app/src/main/java/ebookfrenzy/com/sampleapplication/GcmIntentService.java
@@ -0,0 +1,50 @@
+package ebookfrenzy.com.sampleapplication;
+
+/**
+ * Created by Peru on 14-12-2015.
+ */
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import android.app.IntentService;
+import android.content.Intent;
+import android.widget.Toast;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class GcmIntentService extends IntentService {
+
+    public GcmIntentService() {
+        super("GcmIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Bundle extras = intent.getExtras();
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+        // The getMessageType() intent parameter must be the intent you received
+        // in your BroadcastReceiver.
+        String messageType = gcm.getMessageType(intent);
+
+        if (extras != null && !extras.isEmpty()) {  // has effect of unparcelling Bundle
+            // Since we're not using two way messaging, this is all we really to check for
+            if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
+                Logger.getLogger("GCM_RECEIVED").log(Level.INFO, extras.toString());
+
+                showToast(extras.getString("message"));
+            }
+        }
+        GcmBroadcastReceiver.completeWakefulIntent(intent);
+    }
+
+    protected void showToast(final String message) {
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
